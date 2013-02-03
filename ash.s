5e298e7bd67dc63c7c4fa311ece7a2bbfad2a1bf
Sean Williams <unixed@gmail.com>
2013-02-03 09:07:43 -0800

    Accept one cmdline arg

--- ash.s
+++ ash.s
@@ -1,18 +1,44 @@
-.data
-str: .ascii "Hello world!\n"
+.text
+	prompt: .ascii ">: "
+	.equ SYS_WRITE, 1
+	.equ SYS_EXIT, 60
 
 .text
-.globl _start
+	.globl _start
+
+strlen:
+	pushq %rbp
+	movq %rsp, %rbp
+	movq 16(%rbp), %rdi
+	xorq %rax, %rax
+	xorq %rcx, %rcx
+	notq %rcx
+	cld
+	repne scasb
+	notq %rcx
+	decq %rcx
+	movq %rcx, %rax
+	leave
+	ret
 
 _start:
-   pushq %rbp
-   movq %rsp, %rbp
-   movq $13, %rdx
-   leaq str, %rcx
-   movq $0, %rbx
-   movq $4, %rax
-   int $0x80
+	pushq %rbp
+	movq %rsp, %rbp
+	pushq 24(%rbp)
+	call strlen
+	movq %rax, %rdx
+	movq 24(%rbp), %rsi
+	movq $1, %rdi
+	movq $SYS_WRITE, %rax
+	syscall
 
-   xorq %rbx, %rbx
-   movq $1, %rax
-   int $0x80
+_exit_without_error:
+	movq $0, %rdi
+	jmp _exit
+
+_exit_with_error:
+	movq $1, %rdi
+
+_exit:
+	movq $SYS_EXIT, %rax
+	syscall
